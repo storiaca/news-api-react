3a9d9f2e777129fc9b8594dcfeb016f7c0fd5195
Aleksandar Ristic <storiaca@gmail.com>
2024-04-22 11:54:36 +0200

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl text-center text-stone-800">React News App</h1>
+    </div>
+  );
+}
+
+export default App;
